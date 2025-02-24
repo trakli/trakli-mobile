b3c30126299c08e5c330d7e7c5ba714207728244
Wuyika <wuyika.serge@gmail.com>
2025-02-24 02:06:00 +0100

    fix(analyze-code): Fix flutter analyze

--- lib/presentation/utils/forms/add_category_form.dart
+++ lib/presentation/utils/forms/add_category_form.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:trakli/gen/assets.gen.dart';
 import 'package:trakli/gen/translations/codegen_loader.g.dart';
 import 'package:trakli/presentation/utils/app_navigator.dart';
