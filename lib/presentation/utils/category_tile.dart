56852c5d32bc35d852d5fa4429d623496ccb49ea
Wuyika <wuyika.serge@gmail.com>
2026-03-18 18:21:07 +0100

    enh(UI): UI fixes for heroicons and onboarding pageview

--- lib/presentation/utils/category_tile.dart
+++ lib/presentation/utils/category_tile.dart
@@ -1,14 +1,14 @@
 import 'dart:math' as math;
 
+import 'package:currency_picker/currency_picker.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:trakli/gen/translations/codegen_loader.g.dart';
-import 'package:currency_picker/currency_picker.dart';
 import 'package:trakli/core/utils/currency_formater.dart';
 import 'package:trakli/domain/entities/category_entity.dart';
 import 'package:trakli/gen/assets.gen.dart';
+import 'package:trakli/gen/translations/codegen_loader.g.dart';
 import 'package:trakli/presentation/utils/dialogs/pop_up_dialog.dart';
 import 'package:trakli/presentation/utils/enums.dart';
 import 'package:trakli/presentation/utils/helpers.dart';
@@ -96,144 +96,136 @@ class _CategoryTileState extends State<CategoryTile> {
                   ),
                 )),
             Expanded(
-              child: Column(
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 4.h,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.category.name,
-                            style: TextStyle(
-                              fontSize: 14.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.category.name,
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(height: 4.h),
-                          Text(
-                            widget.showStat
-                                ? "${widget.transactionCount} ${LocaleKeys.transactionsIn.tr()} ${widget.walletCount} ${LocaleKeys.wallets.tr()}"
-                                : widget.category.description ??
-                                    LocaleKeys.noDescription.tr(),
-                            style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                        SizedBox(height: 4.h),
+                        Text(
+                          widget.showStat
+                              ? "${widget.transactionCount} ${LocaleKeys.transactionsIn.tr()} ${widget.walletCount} ${LocaleKeys.wallets.tr()}"
+                              : widget.category.description ??
+                                  LocaleKeys.noDescription.tr(),
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                  if (widget.onEdit != null || widget.onDelete != null)
+                    Container(
+                      width: 36.w,
+                      height: 36.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.r),
+                        color: widget.accentColor.withValues(alpha: 0.2),
+                      ),
+                      child: PopupMenuButton(
+                        icon: SvgPicture.asset(
+                          height: 20.h,
+                          width: 20.w,
+                          Assets.images.more,
+                          colorFilter: ColorFilter.mode(
+                            widget.accentColor,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                        itemBuilder: (context) {
+                          return [
+                            if (widget.onEdit != null)
+                              PopupMenuItem(
+                                onTap: widget.onEdit,
+                                height: 40.h,
+                                child: Row(
+                                  spacing: 8.w,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(6),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Theme.of(context)
+                                            .primaryColor
+                                            .withAlpha(50),
+                                      ),
+                                      child: SvgPicture.asset(
+                                        height: 16.h,
+                                        width: 16.w,
+                                        Assets.images.edit2,
+                                        colorFilter: ColorFilter.mode(
+                                          Theme.of(context).primaryColor,
+                                          BlendMode.srcIn,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(LocaleKeys.edit.tr()),
+                                  ],
+                                ),
+                              ),
+                            if (widget.onDelete != null)
+                              PopupMenuItem(
+                                onTap: _showDeleteConfirmation,
+                                height: 40.h,
+                                child: Row(
+                                  spacing: 8.w,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(6),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.redAccent.withAlpha(50),
+                                      ),
+                                      child: SvgPicture.asset(
+                                        height: 16.h,
+                                        width: 16.w,
+                                        Assets.images.trash,
+                                        colorFilter: const ColorFilter.mode(
+                                          Colors.redAccent,
+                                          BlendMode.srcIn,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(LocaleKeys.delete.tr()),
+                                  ],
+                                ),
+                              ),
+                          ];
+                        },
+                      ),
+                    )
+                  else if (widget.showValue)
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: widget.accentColor,
+                          fontSize: 16.sp,
+                        ),
+                        text: widget.category.type.name == 'expense' ? "-" : "",
+                        children: [
+                          TextSpan(
+                            text: CurrencyFormater.formatAmountWithSymbol(
+                              context,
+                              widget.amount ?? randomValue.toDouble(),
+                              currency: widget.currency,
+                            ),
                           ),
                         ],
                       ),
-                      if (widget.onEdit != null || widget.onDelete != null)
-                        Container(
-                          width: 36.w,
-                          height: 36.w,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.r),
-                            color: widget.accentColor.withValues(alpha: 0.2),
-                          ),
-                          child: PopupMenuButton(
-                            icon: SvgPicture.asset(
-                              height: 20.h,
-                              width: 20.w,
-                              Assets.images.more,
-                              colorFilter: ColorFilter.mode(
-                                widget.accentColor,
-                                BlendMode.srcIn,
-                              ),
-                            ),
-                            itemBuilder: (context) {
-                              return [
-                                if (widget.onEdit != null)
-                                  PopupMenuItem(
-                                    onTap: widget.onEdit,
-                                    height: 40.h,
-                                    child: Row(
-                                      spacing: 8.w,
-                                      children: [
-                                        Container(
-                                          padding: const EdgeInsets.all(6),
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            color: Theme.of(context)
-                                                .primaryColor
-                                                .withAlpha(50),
-                                          ),
-                                          child: SvgPicture.asset(
-                                            height: 16.h,
-                                            width: 16.w,
-                                            Assets.images.edit2,
-                                            colorFilter: ColorFilter.mode(
-                                              Theme.of(context).primaryColor,
-                                              BlendMode.srcIn,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(LocaleKeys.edit.tr()),
-                                      ],
-                                    ),
-                                  ),
-                                if (widget.onDelete != null)
-                                  PopupMenuItem(
-                                    onTap: _showDeleteConfirmation,
-                                    height: 40.h,
-                                    child: Row(
-                                      spacing: 8.w,
-                                      children: [
-                                        Container(
-                                          padding: const EdgeInsets.all(6),
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            color:
-                                                Colors.redAccent.withAlpha(50),
-                                          ),
-                                          child: SvgPicture.asset(
-                                            height: 16.h,
-                                            width: 16.w,
-                                            Assets.images.trash,
-                                            colorFilter: const ColorFilter.mode(
-                                              Colors.redAccent,
-                                              BlendMode.srcIn,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(LocaleKeys.delete.tr()),
-                                      ],
-                                    ),
-                                  ),
-                              ];
-                            },
-                          ),
-                        )
-                      else if (widget.showValue)
-                        RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                              color: widget.accentColor,
-                              fontSize: 16.sp,
-                            ),
-                            text: widget.category.type.name == 'expense'
-                                ? "-"
-                                : "",
-                            children: [
-                              TextSpan(
-                                text: CurrencyFormater.formatAmountWithSymbol(
-                                  context,
-                                  widget.amount ?? randomValue.toDouble(),
-                                  currency: widget.currency,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      else
-                        SvgPicture.asset(
-                          Assets.images.arrowRight,
-                          width: 24.w,
-                          height: 24.h,
-                        ),
-                    ],
-                  ),
+                    )
+                  else
+                    SvgPicture.asset(
+                      Assets.images.arrowRight,
+                      width: 24.w,
+                      height: 24.h,
+                    ),
                 ],
               ),
             ),
